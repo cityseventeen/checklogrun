6298a6bb576a2a96f8e3470d34f468417b022a6e
CitySeventeen <raffaelebalsamo91@gmail.com>
2024-10-14 22:50:42 +0200

    modified filtered methods with adding right function

--- src/fft.js
+++ src/fft.js
@@ -14,17 +14,25 @@ class DFT{
     }
     static get valid_input_for_checklogrun(){return undefined;}
     static get valid_input_for_methods_of_checklogrun(){return ()=>{};}
-    static get all_methods_that_return_object(){
-        const method_to_exclude = data.functionality_list.getFunction;
+    static all_methods_except_for(...methods_to_exclude){
         const list_functionality_from_enums = data.functionality_list.enums;
         const filtered_methods = list_functionality_from_enums
-            .filter(enum_value => {return enum_value !== method_to_exclude});
+            .filter(enum_value => {return !(methods_to_exclude.includes(enum_value))});
         
         const only_name_of_methods = ((list)=>{
             return list.map(enum_value => {return enum_value.key})
         })(filtered_methods);
 
         return only_name_of_methods;
+    }
+    static get all_methods_that_return_object(){
+        const method_to_exclude = data.functionality_list.getFunction;
+        const only_name_of_methods = DFT.all_methods_except_for(method_to_exclude);
+
+        return only_name_of_methods;
+    }
+    static get all_methods_that_require_main_to_be_called_first(){
+
     }
 }
 
